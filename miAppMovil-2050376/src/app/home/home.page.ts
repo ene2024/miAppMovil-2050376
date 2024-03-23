7b6179e1a8833ee72cfcb8ff91e27d45e2d29d5c
Jesm31 <sjared912@gmail.com>
2024-03-23 16:11:25 -0600

    Databinding

--- miAppMovil-2050376/src/app/home/home.page.ts
+++ miAppMovil-2050376/src/app/home/home.page.ts
@@ -17,4 +17,6 @@ export class HomePage {
 
   imgUrl: string = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FClub_de_F%25C3%25BAtbol_Monterrey&psig=AOvVaw1fkuUgX5Jwc_z4jQsMGmuT&ust=1710981845700000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjT2JvOgYUDFQAAAAAdAAAAABAE"
 
+
+  nombreAlumno : string =' ';
 }
